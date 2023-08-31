7041ba2118f94064b35e561112202eedc56acb72
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-08-30 22:25:17 -0400

    Update Raven.java

--- blackbox-test-records/src/main/java/io/avaje/recordbuilder/test/Raven.java
+++ blackbox-test-records/src/main/java/io/avaje/recordbuilder/test/Raven.java
@@ -5,7 +5,12 @@ import io.avaje.recordbuilder.RecordBuilder;
 
 @RecordBuilder
 public record Raven(
-    int ap, String coreName, Weapon rArm, Weapon lArm, Weapon rShoulder, Weapon lShoulder) {
+    int ap,
+    String coreName,
+    Weapon rArm,
+    @DefaultInit("new Raven.Weapon(0, \"pilebunker\", DamageType.EXPLOSIVE)") Weapon lArm,
+    Weapon rShoulder,
+    Weapon lShoulder) {
 
   @RecordBuilder
   public record Weapon(@DefaultInit("500") int ammo, String name, DamageType type) {}
