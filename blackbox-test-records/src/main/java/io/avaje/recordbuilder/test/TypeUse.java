5df9a1e3e667ce9552921fd3438640cdb78eeeb8
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-09-03 23:36:25 -0400

    Update TypeUse.java

--- blackbox-test-records/src/main/java/io/avaje/recordbuilder/test/TypeUse.java
+++ blackbox-test-records/src/main/java/io/avaje/recordbuilder/test/TypeUse.java
@@ -9,4 +9,9 @@ import io.avaje.validation.constraints.NotEmpty;
 
 @RecordBuilder
 public record TypeUse(
-    @NotEmpty @NotBlank Map<@NotBlank(groups = Weapon.class) String, @NotEmpty(groups = Weapon.class) Map<@NotBlank(groups = Weapon.class) Weapon, @NotBlank Raven>> map) {}
+    @NotEmpty @NotBlank
+        Map<
+                @NotBlank(groups = Weapon.class) String,
+                @NotEmpty(groups = Weapon.class) Map<
+                    @NotBlank(groups = Weapon.class) Weapon, @NotBlank Raven>>
+            map) {}
