2be049a92ec026a3bc6892ee49690b9a8b227370
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2025-09-23 22:29:40 -0400

    Update Hornet.java

--- blackbox-test-records/src/main/java/io/avaje/recordbuilder/test/pkginfo/Hornet.java
+++ blackbox-test-records/src/main/java/io/avaje/recordbuilder/test/pkginfo/Hornet.java
@@ -1,5 +1,14 @@
 package io.avaje.recordbuilder.test.pkginfo;
 
 public record Hornet(Silk silk) {
-  public record Silk(String song) {}
+
+  HornetBuilder builder() {
+    return HornetBuilder.builder();
+  }
+
+  public record Silk(String song) {
+    Hornet$SilkBuilder builder() {
+      return Hornet$SilkBuilder.builder();
+    }
+  }
 }
